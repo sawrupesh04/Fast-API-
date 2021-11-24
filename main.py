51ef59a4e13beec08e9993b715408ba245fd6827
sawrupesh04 <rupeshsaw786@gmail.com>
2021-11-24 22:07:12 +0530

    items url added

--- main.py
+++ main.py
@@ -7,3 +7,6 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
